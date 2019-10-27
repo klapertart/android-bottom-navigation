eff5b034b92409fce8ada5a734f65676f1c13880
Klapertart <klapertartinc@gmail.com>
2019-10-27 16:27:47 +0700

    update

--- app/src/main/java/com/ostrue/app/bottomtab/MainActivity.java
+++ app/src/main/java/com/ostrue/app/bottomtab/MainActivity.java
@@ -2,14 +2,22 @@ package com.ostrue.app.bottomtab;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity
+        extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener,
+        PostFragment.OnFragmentInteractionListener,
+        ExploreFragment.OnFragmentInteractionListener,
+        UserFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,23 +25,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView btNav = findViewById(R.id.bottom_navigation);
-        btNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-                    case R.id.nav_explore :
-                        Toast.makeText(MainActivity.this, "Explore", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.nav_post :
-                        Toast.makeText(MainActivity.this, "Post", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.nav_user :
-                        Toast.makeText(MainActivity.this, "User", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-
-                return true;
-            }
-        });
+        btNav.setOnNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+        Fragment fragment = null;
+
+        switch (item.getItemId()){
+            case R.id.nav_explore :
+                fragment = new ExploreFragment();
+                break;
+            case R.id.nav_post :
+                fragment = new PostFragment();
+                break;
+            case R.id.nav_user :
+                fragment = new UserFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container,fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    public void onPostFragmentInteraction(Uri uri){}
+    public void onExploreFragmentInteraction(Uri uri){}
+    public void onUserFragmentInteraction(Uri uri){}
 }
